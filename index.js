ff084ec32c1b09200608e3956af1a485767a67ff
Gabriel Oliveira <gabriel.olv2002@gmail.com>
2021-06-08 21:19:14 -0300

    continue

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+client.login('ODQ5NDI4ODI0MzUzMDEzNzkx.YLbCPQ.i3xOC3aPiMJBCZB_9P8KhR9ZPE8');
 
-client.login('token');
 
+client.on('ready',() => {
+    new Discord.Message(client,'Teste','notifications');
+});
 
 
 client.on('message', msg => {
-    for (var i=0; i<)
+    if (msg.content === '!ping'){
+        msg.channel.send('@here, pong!');
+    }
 });
+
