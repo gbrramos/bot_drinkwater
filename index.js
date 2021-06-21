f9c5fd1425d9c0b25111b53d82e2dc9a17f7bbb0
Gabriel Oliveira <gabriel.olv2002@gmail.com>
2021-06-21 14:15:21 -0300

    finish tests

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-client.login('token');
+const token = 'ODQ5NDI4ODI0MzUzMDEzNzkx.YLbCPQ.HaevizH5H5nydVH-CJoG5v9g7Ag';
+client.login(token);
 
 
-client.on('ready',() => {
-    new Discord.Message(client,'Teste','notifications');
-});
-
-
-client.on('message', msg => {
-    if (msg.content === '!ping'){
-        msg.channel.send('@here, pong!');
-    }
+client.on('ready', () => {
+    h = new Date();
+    console.log(h.getHours());
+    console.log('Drinkwater: Estou Online!');
 });
 
