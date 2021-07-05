1a7059306aeb57186aaa2e0bab4ce654dfee9c29
Gabriel Oliveira <gabriel.olv2002@gmail.com>
2021-07-05 12:54:46 -0300

    finish

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = 'ODQ5NDI4ODI0MzUzMDEzNzkx.YLbCPQ.HaevizH5H5nydVH-CJoG5v9g7Ag';
+const token = 'ODQ5NDI4ODI0MzUzMDEzNzkx.YLbCPQ.OZEvxW0nEA0ThIguyb0gBX7Qx4w';
 client.login(token);
 
 
 client.on('ready', () => {
-    h = new Date();
-    console.log(h.getHours());
     console.log('Drinkwater: Estou Online!');
+    h = new Date();
+    var i = 0;
+    while (i <= 5) {
+        hora = h.getHours();
+        minuto = h.getUTCMinutes();
+        if (minuto < 10) {
+            minuto = '0' + minuto;
+        }
+        fullHour = hora + ":" + minuto;
+        // console.log(fullHour);
+        if (fullHour == '11:00' || fullHour == '13:30' || fullHour == '14:00' || fullHour == '14:30' || fullHour == '15:00' || fullHour == '16:00') {
+            x = true;
+            while(x == true){
+                client.channels.cache.get(`854132041002778665`).send('Vai beber água mamaco');
+                x = false;
+            }
+            i = i + 1;
+        }
+    }
 });
-
